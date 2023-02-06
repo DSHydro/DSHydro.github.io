ffc60cfe0d83659357f5abae2be53767a4a51ec1
StefanTodoran <stefan.alex4@gmail.com>
2023-02-05 23:07:19 -0800

    team page improvement, mobile menu fix, footer adjustment on mobile, scroll top improvement

--- js/index.js
+++ js/index.js
@@ -21,6 +21,15 @@
       const id = fish[i].id;
       setTimeout(() => { updateFish(id); }, random(0, 3) * 1000);
     }
+
+    // The scroll top button should work even without js enabled so it uses
+    // an anchor, however that appends an ugly '#scroll-top' to the url so
+    // for users with js enabled, we do this:
+    const scroll_btn = document.getElementById('scroll-top-button');
+    scroll_btn.addEventListener('click', (evt) => {
+      window.scrollTo({top: '0'});
+      evt.preventDefault();
+    });
   }
 
   /**
